704b3cf42032bd560e71fadd8f6012d1ea175aa9
Andre Casimiro <andrecasimio@gmail.com>
2024-07-18 15:30:37 -0300

    create first route

---
+++ Java/todolist/src/main/java/br/com/andre_casimiro/github/todolist/controller/MFController.java
@@ -0,0 +1,20 @@
+package br.com.andre_casimiro.github.todolist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/mfRoute")
+// http://localhost:8080/mfRoute
+public class MFController {
+
+    @GetMapping("/hello")
+    public String firstMensage(){
+        return "hello world spring boot";
+    }
+    
+    
+}
